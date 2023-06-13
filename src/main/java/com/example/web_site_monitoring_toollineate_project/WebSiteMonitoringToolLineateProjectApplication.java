580592210b374c26da991fc9821c627c8a371446
levani <lpilp21@freeuni.edu.ge>
2023-06-13 15:42:35 +0400

    Initial commit

---
+++ src/main/java/com/example/web_site_monitoring_toollineate_project/WebSiteMonitoringToolLineateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_site_monitoring_toollineate_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSiteMonitoringToolLineateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSiteMonitoringToolLineateProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_site_monitoring_toollineate_project/WebSiteMonitoringToolLineateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_site_monitoring_toollineate_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSiteMonitoringToolLineateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
